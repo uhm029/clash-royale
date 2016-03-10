50869640f5e09323ef0c5e600e5b0658993abdc5
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2016-03-10 14:32:29 +0800

    Splitted 'card-data-troops.go' from 'card.go'

--- lib/card-data-troops.go
+++ lib/card-data-troops.go
@@ -1,28 +1,5 @@
 package lib
 
-type Card map[Attribute]interface{}
-
-func (c *Card) GetValue(attr Attribute) interface{} {
-	if cattr, ok := (*c)[attr]; ok {
-		return cattr
-	}
-	return (*c)[RARITY].(*Rarity).GetValue(attr);
-}
-
-func (c *Card) GetFormattedValue(fattr *FixedAttribute) string {
-	if value := c.GetValue(fattr); value != nil {
-		return fattr.FormatValue(value)
-	}
-	return ""
-}
-
-func (c *Card) GetFormattedValues(uattr *UpgradableAttribute) []string {
-	if values := c.GetValue(uattr); values != nil {
-		return uattr.FormatValues(values)
-	}
-	return nil
-}
-
 var (
 	KNIGHT = Card{
 		NAME:   "Knight",
@@ -406,29 +383,3 @@ var (
 		DTIME:  3,
 	}
 )
-
-var CARDS = [...]Card{
-	KNIGHT,
-	ARCHERS,
-	BOMBER,
-	GOBLINS,
-	SPEAR_GOBLINS,
-	SKELETONS,
-	MINIONS,
-	BARBARIANS,
-	MINION_HORDE,
-	GIANT,
-	MUSKETEER,
-	MINI_PEKKA,
-	VALKYRIE,
-	HOG_RIDER,
-	WIZARD,
-	WITCH,
-	SKELETON_ARMY,
-	BABY_DRAGON,
-	PRINCE,
-	GIANT_SKELETON,
-	BALLOON,
-	PEKKA,
-	GOLEM,
-}
