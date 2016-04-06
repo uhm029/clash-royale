d9c385ee6399627f07b861454fcab8dd599e340f
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2016-04-06 19:27:27 +0800

    Updated values

--- lib/generator.go
+++ lib/generator.go
@@ -4,8 +4,8 @@ var (
 	V1000R = []int{1000, 1100, 1210, 1330, 1460, 1600, 1760, 1930, 2120, 2330, 2560, 2810}
 )
 
-func generateHp(value interface{}) []int {
-	baseValue := value.(int)
+func generateHp(baseHp interface{}) []int {
+	baseValue := baseHp.(int)
 	values := make([]int, len(V1000R))
 	for i, v := range V1000R {
 		values[i] = v * baseValue / 1000
@@ -13,6 +13,16 @@ func generateHp(value interface{}) []int {
 	return values
 }
 
-func generateDam(value interface{}) []int {
-	return generateHp(value)
+func generateDam(baseDam interface{}) []int {
+	return generateHp(baseDam)
 }
+
+/*
+func generateDps(baseDam interface{}, hitSpeed float64) []int {
+	values := generateDam(baseDam)
+	for i, v := range values {
+		values[i] = int(float64(v)/hitSpeed + 0.5)
+	}
+	return values
+}
+*/
